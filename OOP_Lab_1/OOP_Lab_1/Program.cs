fce1f2041d14f8ce94372ba52c896f781315a7d6
Rostyslav <excoreky@gmail.com>
2020-09-17 23:09:10 +0300

    Lab 1 - first commit

---
+++ OOP_Lab_1/OOP_Lab_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OOP_Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("Enter a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Enter b: ");
+            b = int.Parse(Console.ReadLine());
+
+            if (b < a)
+            {
+                Console.WriteLine("b should be equal or greater than a");
+                return;
+            }
+
+            Random r = new Random();
+            int n = (r.Next() % (b - a + 1)) + a;
+        }
+    }
+}
---
+++ OOP_Lab_1/OOP_Lab_1/TTriangle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace OOP_Lab_1
+{
+    public class TTriangle
+    {
+        protected double a, b, c; // sides
+
+        public TTriangle(double a, double b, double c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                throw new ArgumentException("Sides should be of positive length");
+            }
+            if (a >= b + c || b >= a + c || c >= a + b)
+            {
+                throw new ArgumentException("Bad triangle");
+            }
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public double getA()
+        {
+            return a;
+        }
+
+        public double getB()
+        {
+            return b;
+        }
+
+        public double getC()
+        {
+            return c;
+        }
+
+        public TTriangle()
+        {
+        }
+    }
+}
