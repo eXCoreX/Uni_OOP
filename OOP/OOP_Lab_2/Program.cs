23a3edf40c6e16f339cf82eeeb786e12dde8a5c2
Rostyslav <excoreky@gmail.com>
2020-10-01 15:32:44 +0300

    Lab 2 - second task done

--- OOP/OOP_Lab_2/Program.cs
+++ OOP/OOP_Lab_2/Program.cs
@@ -10,6 +10,25 @@ namespace OOP_Lab_2
             Console.WriteLine(rect);
             rect.InputRectangle();
             Console.WriteLine(rect);
+
+            TCircle circle = new TCircle();
+            circle.InputCircle();
+            Console.WriteLine(circle);
+            TCircle circle2 = new TCircle(circle);
+            Console.WriteLine(circle2);
+            Console.WriteLine(circle2.Area);
+            Console.WriteLine(circle2.GetSectorArea(Math.PI/3.0));
+
+            TSphere sphere = new TSphere(3.5);
+            Console.WriteLine(sphere.Volume);
+            TCircle sphereAsCircle = sphere as TCircle;
+            Console.WriteLine(sphereAsCircle);
+            Console.WriteLine(sphere.Length);
+
+            TCircle circle3 = circle + sphereAsCircle;
+            Console.WriteLine(circle3);
+            Console.WriteLine(circle3 * 9);
+            Console.WriteLine(circle3 - 1.5 * sphereAsCircle);
             Console.ReadKey();
         }
     }
