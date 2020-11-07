54af1d8f05689a8d3fe98130917c182a40082ae2
Rostyslav <excoreky@gmail.com>
2020-11-07 21:19:15 +0200

    Lab 3 - 2nd task done

---
+++ OOP/OOP_Lab_3/MyTime.cs
@@ -0,0 +1,220 @@
+﻿using System;
+namespace OOP_Lab_3
+{
+    public class MyTime
+    {
+        // Fields
+
+        protected int hour, minute, second;
+
+        // Properties
+
+        public int Hour
+        {
+            get
+            {
+                return hour;
+            }
+            set
+            {
+                hour = value;
+                Normalize();
+            }
+        }
+
+
+        public int Minute
+        {
+            get
+            {
+                return minute;
+            }
+            set
+            {
+                minute = value;
+                Normalize();
+            }
+        }
+
+
+        public int Second
+        {
+            get
+            {
+                return second;
+            }
+            set
+            {
+                second = value;
+                Normalize();
+            }
+        }
+
+
+        // Constructors
+
+        public MyTime()
+        {
+            hour = 0;
+            minute = 0;
+            second = 0;
+        }
+
+
+        public MyTime(MyTime other)
+        {
+            hour = other.hour;
+            minute = other.minute;
+            second = other.second;
+        }
+
+
+        public MyTime(int secondsFromMidnight)
+        {
+            Second = secondsFromMidnight;
+        }
+
+
+        public MyTime(int hour, int minute, int second)
+        {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+            Normalize();
+        }
+
+
+        // Protected Methods
+
+        protected void HMSFromSeconds(int s)
+        {
+            s %= (24 * 60 * 60);
+            s += (24 * 60 * 60);
+            s %= (24 * 60 * 60);
+
+            hour = s / 60 / 60;
+            minute = (s / 60) % 60;
+            second = s % 60;
+        }
+
+
+        protected void Normalize()
+        {
+            int s = TimeSinceMidnight();
+            HMSFromSeconds(s);
+        }
+
+
+        // Static Methods
+
+        public static int Difference(MyTime t1, MyTime t2)
+        {
+            return t1.TimeSinceMidnight() - t2.TimeSinceMidnight();
+        }
+
+
+        public static int operator -(MyTime t1, MyTime t2)
+        {
+            return t1.TimeSinceMidnight() - t2.TimeSinceMidnight();
+        }
+
+
+        public static bool operator <(MyTime t1, MyTime t2)
+        {
+            return t1.TimeSinceMidnight() < t2.TimeSinceMidnight();
+        }
+
+
+        public static bool operator >(MyTime t1, MyTime t2)
+        {
+            return t1.TimeSinceMidnight() > t2.TimeSinceMidnight();
+        }
+
+
+        public static bool operator <=(MyTime t1, MyTime t2)
+        {
+            return t1.TimeSinceMidnight() <= t2.TimeSinceMidnight();
+        }
+
+
+        public static bool operator >=(MyTime t1, MyTime t2)
+        {
+            return t1.TimeSinceMidnight() >= t2.TimeSinceMidnight();
+        }
+
+
+        // Public Methods
+
+        public int TimeSinceMidnight()
+        {
+            return Hour * 60 * 60 + Minute * 60 + Second;
+        }
+
+
+        public MyTime AddOneSecond()
+        {
+            return new MyTime(Hour, Minute, Second + 1);
+        }
+
+
+        public MyTime AddOneMinute()
+        {
+            return new MyTime(Hour, Minute + 1, Second);
+        }
+
+
+        public MyTime AddOneHour()
+        {
+            return new MyTime(Hour + 1, Minute, Second);
+        }
+
+
+        public MyTime AddSeconds(int s)
+        {
+            return new MyTime(Hour, Minute, Second + s);
+        }
+
+
+        public string WhatLesson()
+        {
+            if (this < new MyTime(8, 0, 0))
+            {
+                return "пари ще не почались";
+            }
+            MyTime tmp = new MyTime(this);
+            tmp.Hour -= 8;
+            int les_num = 1;
+            while (les_num < 5 && (tmp >= new MyTime(1, 40, 0))) // Counting lessons and breaks 1st through 4th
+            {
+                les_num++;
+                tmp.Hour -= 1;
+                tmp.Minute -= 40;
+            }
+            if (les_num == 5 && (tmp >= new MyTime(1, 30, 0))) // after 5th lesson break is 10mins long
+            {
+                les_num++;
+                tmp.Hour -= 1;
+                tmp.Minute -= 30;
+            }
+
+            if (tmp < new MyTime(1, 20, 0))
+            {
+                return $"{les_num}-{(les_num == 3 ? "я" : "а")} пара";
+            }
+            else if (les_num < 6)
+            {
+                return $"перерва між {les_num}-ю та {les_num + 1}-ю парами";
+            }
+            else
+            {
+                return "пари вже скінчились";
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return string.Format("{0}:{1:d2}:{2:d2}", Hour, Minute, Second);
+        }
+    }
+}
--- OOP/OOP_Lab_3/Program.cs
+++ OOP/OOP_Lab_3/Program.cs
@@ -4,7 +4,7 @@ namespace OOP_Lab_3
 {
     class Program
     {
-        static void Main(string[] args)
+        static void TaskA()
         {
             double[,] mtx1 =
             {
@@ -31,10 +31,10 @@ namespace OOP_Lab_3
             Console.WriteLine(c);
             Console.WriteLine();
             MyMatrix m1 = new MyMatrix(mtx2);
-            MyMatrix m2 = new MyMatrix(mtx3); 
-            Console.WriteLine(m1*m2);
+            MyMatrix m2 = new MyMatrix(mtx3);
+            Console.WriteLine(m1 * m2);
             Console.WriteLine();
-            Console.WriteLine(m2*m1);
+            Console.WriteLine(m2 * m1);
             Console.WriteLine();
 
             double[,] mtx4 =
@@ -48,6 +48,63 @@ namespace OOP_Lab_3
             M1.TransponeMe();
             Console.WriteLine(M1);
             return;
+        }
+
+
+        static void TaskB()
+        {
+            MyTime t = new MyTime(9, 2, 30);
+            Console.WriteLine(t);
+
+            Console.WriteLine("TimeSinceMidnight:");
+            Console.WriteLine(t.TimeSinceMidnight());
+            Console.WriteLine(new MyTime(24 * 60 * 60 - 1));
+
+            Console.WriteLine("Add one (s/m/h):");
+            t = t.AddOneSecond();
+            Console.WriteLine(t);
+            t = t.AddOneMinute();
+            Console.WriteLine(t);
+            t = t.AddOneHour();
+            Console.WriteLine(t);
+
+            Console.WriteLine("AddSeconds:");
+            t = t.AddSeconds(-40000);
+            Console.WriteLine(t);
+
+            Console.WriteLine(new MyTime(23, 59, 59).AddSeconds(2));
+
+            Console.WriteLine("Difference:");
+            Console.WriteLine(MyTime.Difference(t, new MyTime(4, 20, 0)));
+
+            MyTime t1 = new MyTime(7, 59, 59);
+            Console.WriteLine("What lesson {0}: {1}", t1, t1.WhatLesson());
+            MyTime t2 = new MyTime(8, 0, 0);
+            Console.WriteLine("What lesson {0}: {1}", t2, t2.WhatLesson());
+            MyTime t3 = new MyTime(9, 19, 59);
+            Console.WriteLine("What lesson {0}: {1}", t3, t3.WhatLesson());
+            MyTime t4 = new MyTime(9, 20, 0);
+            Console.WriteLine("What lesson {0}: {1}", t4, t4.WhatLesson());
+            MyTime t5 = new MyTime(17, 29, 59);
+            Console.WriteLine("What lesson {0}: {1}", t5, t5.WhatLesson());
+            MyTime t6 = new MyTime(17, 30, 0);
+            Console.WriteLine("What lesson {0}: {1}", t6, t6.WhatLesson());
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Task(A/B): ");
+            string ans = Console.ReadLine().Trim().ToLower();
+            switch (ans)
+            {
+                case "a":
+                    TaskA();
+                    break;
+                case "b":
+                    TaskB();
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
